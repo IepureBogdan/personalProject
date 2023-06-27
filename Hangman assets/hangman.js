9039e3f2c1bce26d85e0a049cd253b5767cfc52e
IepureBogdan <b0gdan_iepure@yahoo.com>
2023-06-27 19:55:08 +0300

    firstCommit

---
+++ Hangman assets/hangman.js
@@ -0,0 +1,166 @@
+
+// assigning variables
+
+
+const letters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+const countries = ['DENMARK' , 'GERMANY' , 'FRANCE' , 'ITALY' , 'HUNGARY' , 'POLAND' , 'SPAIN', 'ROMANIA', 'ALBANIA'];
+
+
+const container= document.getElementById('container');
+const hangmanDrawing= document.getElementById('hangmanDrawing');
+const letterContainer=document.getElementById('letterContainer');
+const optionsContainer = document.getElementById('optionsContainer');
+const resetBtn = document.getElementById('resetBtn');
+let gameWord = document.createElement('div');
+
+
+let randomCountry = '';
+let wrongAnswer = 0;
+let maxWrongAnswer = 6;
+let gameLetter = [];
+let gamerChoice = [];
+let buttonLetter = '';
+
+
+// Buttons whith letters 
+
+let letter
+for(let i=0; i<letters.length;i++){
+    let buttonA = document.createElement('div');
+    letter = letters[i];
+    buttonA.innerHTML = `<button id="${letter}" class="letterBtn" onclick = "chosenLetter('${letter}')">${letter}</button>`;
+    
+    letterContainer.append(buttonA);
+
+}
+
+
+
+// };
+// generateBtn();
+
+
+// letter Button function  
+
+
+function chosenLetter(chosenLtr) {
+    
+    
+    // buttonLetter = chosenLtr;
+    let letterIndex = randomCountry.indexOf(chosenLtr);
+    document.getElementById(chosenLtr).disabled = true ;
+    
+    
+    if (randomCountry.includes(chosenLtr)) {
+        
+        for (let i = 0; i < randomCountry.length; i++) {
+            if (randomCountry[i] === chosenLtr) {
+              gameLetter[i] = chosenLtr;
+              gamerChoice.push(chosenLtr);
+            }
+          }
+        gameLetter.splice(letterIndex , 1 , chosenLtr);
+        gameWord.innerHTML = gameLetter.join('');
+
+        
+        
+    }
+    
+    else {
+        wrongAnswer += 1
+        document.getElementById('hangmanDrawing').innerHTML = `<img src="assets/picture${wrongAnswer}.png">`;
+    }
+    
+    
+    
+    // test button task
+    // letterContainer.style.backgroundColor = "red";
+    console.log(gamerChoice);
+    console.log(wrongAnswer);
+    console.log(gamerChoice.length);
+    console.log(randomCountry.length);
+    console.log(letterIndex);
+    console.log(gameLetter);
+    
+    // Win / Lose section with setTimeout for better user experience
+
+    if (wrongAnswer === maxWrongAnswer) {
+        setTimeout( () => {
+            alert(`you lost. The answer was ${randomCountry}`);
+            window.location.reload();
+            } , 300 );
+        // alert(`you lost. The answer was ${randomCountry}`);
+        // window.location.reload();
+    } else if (gamerChoice.length === randomCountry.length) {
+        setTimeout( () => {
+            alert('you won');
+            window.location.reload();
+            } , 500 );
+        //  alert('you won');
+    }
+    mistakesCounter.textContent = `Mistakes : ${wrongAnswer} from maximum of ${maxWrongAnswer}`;
+    
+}
+
+
+//  set random country
+
+function randomCtr () {
+    randomCountry = countries[Math.floor(Math.random() * countries.length)];
+};
+randomCtr();
+
+
+function displayWord() {
+    
+    // let gameLetter = document.createElement('p');
+
+    for ( let i = 0 ; i < randomCountry.length; i ++) {
+        let randomLetters = randomCountry.split(i,i+1);
+        randomLetters = ` _ `
+        // '<span class="underline">&nbsp;_&nbsp;</span>'
+        // gameLetter += randomLetters;
+         
+        gameLetter.push(randomLetters);
+    }
+    
+    
+    
+    gameWord.innerHTML = gameLetter.join('');
+    
+    optionsContainer.append(gameWord);
+
+    gameWord.classList.add('randomCountry');
+
+    
+
+}
+ displayWord();
+
+
+
+ const mistakesCounter = document.createElement('div');
+ mistakesCounter.textContent = `Mistakes : ${wrongAnswer} from maximum of ${maxWrongAnswer}`;
+ optionsContainer.append(mistakesCounter);
+ mistakesCounter.classList.add('mistakesCounter');
+
+
+
+// back button
+
+
+document.getElementById('backBtn').addEventListener('click', () => {alert('bet back to main')} );
+
+//  function back() {
+//     alert('bet back to main')
+//  }
+
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,118 @@
+console.log('Hello World');
+
+// variables
+
+const socialsButton = document.getElementById('socialsBtn');
+let clickCounter = 0;
+let facebook = document.getElementById('facebook');
+let instagram = document.getElementById('instagram');
+let linkedin = document.getElementById('linkedin');
+let github = document.getElementById('github');
+let phone = document.getElementById('phone');
+let email = document.getElementById('email');
+let rightSide = document.getElementById('rightSide');
+let clockWeather = document.getElementById('clockWeather');
+let clock = document.getElementById('clock');
+let weather = document.getElementById('weather');
+
+// on click animation on social icons
+
+socialsButton.addEventListener('click' , (showSocials) => {
+    
+    clickCounter++ ; 
+    if (clickCounter % 2 == 0) {
+
+        facebook.style.animation = 'disapear 0.3s cubic-bezier(.3,.11,.63,1.32) reverse';
+        instagram.style.animation = 'disapear 0.6s cubic-bezier(.3,.11,.63,1.32) reverse';
+        linkedin.style.animation = 'disapear 0.9s cubic-bezier(.3,.11,.63,1.32) reverse';
+        github.style.animation = 'disapear 1.2s cubic-bezier(.3,.11,.63,1.32) reverse';
+        phone.style.animation = 'disapear 1.5s cubic-bezier(.3,.11,.63,1.32) reverse';
+        email.style.animation = 'disapear 1.8s cubic-bezier(.3,.11,.63,1.32) reverse';
+
+    } else if (clickCounter % 2 == 1) {
+        facebook.style.animation = 'apear 0.3s cubic-bezier(.3,.11,.63,1.32) forwards';
+     
+        instagram.style.animation = 'apear 0.6s cubic-bezier(.3,.11,.63,1.32) forwards';
+        
+        linkedin.style.animation = 'apear 0.9s cubic-bezier(.3,.11,.63,1.32) forwards';
+        
+        github.style.animation = 'apear 1.2s cubic-bezier(.3,.11,.63,1.32) forwards';
+       
+        phone.style.animation = 'apear 1.5s cubic-bezier(.3,.11,.63,1.32) forwards';
+       
+        email.style.animation = 'apear 1.8s cubic-bezier(.3,.11,.63,1.32) forwards';
+    }
+});
+
+
+setTimeout(() => {
+    rightSide.style.animation = 'rightApear 0.6s ease forwards';
+  },5000);
+
+
+var swiper = new Swiper(".mySwiper", {
+    
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: true,
+  mousewheelControl: true,
+
+
+});
+
+
+// clock and weather requests 
+
+
+// using axios
+
+
+axios.get('https://api.open-meteo.com/v1/forecast?latitude=46.77&longitude=23.60&hourly=temperature_2m,apparent_temperature,weathercode&daily=weathercode,temperature_2m_max,precipitation_sum,rain_sum,precipitation_probability_max&current_weather=true&timezone=GMT')
+
+.then(temp => {
+      console.log(temp);
+      let temperature = temp.data.current_weather.temperature;
+      weather.innerHTML = `${temperature} °C`
+      if (temp.data.daily.precipitation_probability_max[0] < 50) {
+        weather.append('🌞');
+      }
+      else {
+        weather.append('💧');
+      }
+}, err => {
+    console.log(err)
+})
+
+
+
+
+//  using fetch 
+
+
+
+
+function getHour() {
+  fetch('http://worldtimeapi.org/api/timezone/Europe/Bucharest')
+        .then((response) => {
+                return response.json()
+            })
+        .then(hour => {
+
+                let liveHour = hour.datetime.substr(11, 11);
+                
+                clock.innerHTML = liveHour.substr(0,8);
+                
+            })
+}
+
+setInterval(getHour
+  , 1000 );
+  
